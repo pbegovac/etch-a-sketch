1b5836f5741bb81b1e2afa1852e6e179b2bc98a3
pbegovac <69535626+pbegovac@users.noreply.github.com>
2021-01-16 13:30:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
-main();
-
-document.querySelector(".reset").addEventListener("click", () => {
-  location.reload();
-});
-
-function main() {
-  let n = parseInt(prompt("Choose a number", 16)); // prompt for the first time or every time the number is between 0 and 100
-
-  while (n <= 0 || n > 100) {
-    alert("Please choose a number between 1 and 100");
-    n = parseInt(prompt("Choose a number", 16)); // prompt when number is not between 0 and 100
-  }
-
-  const container = document.querySelector(".container");
-  addGrids(n, container);
-  container.addEventListener("mouseover", getColor);
-}
+const container = document.querySelector(".container");
+const output = document.querySelector(".output");
+const range = document.querySelector(".size");
+let n = parseInt(range.value);
 
 function addGrids(n, container) {
+  const sizeContainer = getComputedStyle(container);
+  const width = parseInt(sizeContainer.width);
+  const height = parseInt(sizeContainer.height);
+
   for (i = 0; i < n * n; i++) {
     let square = document.createElement("div");
-    square.style.width = `${500 / n}px`;
-    square.style.height = `${500 / n}px`;
+    square.style.width = `${width / n}px`;
+    square.style.height = `${height / n}px`;
     square.classList.add("square");
     container.appendChild(square);
   }
+  const squares = container.querySelectorAll(".square");
+  squares.forEach((squares) => squares.addEventListener("mouseover", getColor));
 }
 
 function getColor(e) {
@@ -34,3 +26,18 @@ function getColor(e) {
   let red = Math.floor(Math.random() * 256 + 1);
   return (square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`);
 }
+
+//default size
+addGrids(n, container);
+
+//Event listeners
+range.addEventListener("input", () => (output.value = range.value));
+range.addEventListener("mouseup", () => (n = parseInt(range.value)));
+range.addEventListener("click", () => {
+  const squares = container.querySelectorAll(".square");
+  squares.forEach((squares) => squares.remove());
+  addGrids(n, container);
+});
+document.querySelector(".reset").addEventListener("click", () => {
+  location.reload();
+});
